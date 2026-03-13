bc50996a88350a1d20cb072a26f3607935b7b336
user2oo4 <hiepphamm04@gmail.com>
2026-03-13 15:18:35 -0400

    simulation seems to work lol

--- src/game.py
+++ src/game.py
@@ -13,6 +13,7 @@ class Game:
         self.stats = {}
         self.ratings = {}
         self.realized_stats = {}
+        self.game_stats = {} # Simulated stats at the end of the game (2pts, 3pts, ft, tovs, rebounds)
 
     def get_data(self):
         for team in self.teams:
@@ -26,6 +27,8 @@ class Game:
         self.realized_stats[t2] = {}
         self.realized_stats[t1]['adjustment_factor'] = self.ratings[t2]['ratings_adjusted']['defensiveRating'] / 100.0
         self.realized_stats[t2]['adjustment_factor'] = self.ratings[t1]['ratings_adjusted']['defensiveRating'] / 100.0
+        self.game_stats[t1] = {'2pt_made': 0, '3pt_made': 0, 'ft_made': 0, 'tov': 0, 'offensive_rebound': 0, '2pt_attempted': 0, '3pt_attempted': 0, 'ft_attempted': 0, 'off_rebounds': 0, 'def_rebounds': 0}
+        self.game_stats[t2] = {'2pt_made': 0, '3pt_made': 0, 'ft_made': 0, 'tov': 0, 'offensive_rebound': 0, '2pt_attempted': 0, '3pt_attempted': 0, 'ft_attempted': 0, 'off_rebounds': 0, 'def_rebounds': 0}
 
     def calculate_realized_stats(self, adjusted: bool):
         for team, opp in [(self.teams[0], self.teams[1]), (self.teams[1], self.teams[0])]:
@@ -36,9 +39,9 @@ class Game:
             realized['possession_time'] = 20 * 60 / realized['possession']
             realized['tov_rate'] = stats['fourFactors']['turnoverRatio']
             realized['offensive_rebound_rate'] = stats['fourFactors']['offensiveReboundPct']
+            realized['ft_rate'] = stats['fourFactors']['freeThrowRate']
             realized['2pt_distribution'] = stats['twoPointFieldGoals']['attempted'] / stats['fieldGoals']['attempted']
             realized['3pt_distribution'] = stats['threePointFieldGoals']['attempted'] / stats['fieldGoals']['attempted']
-            realized['ft_distribution'] = stats['freeThrows']['attempted'] / stats['fieldGoals']['attempted']
             realized['2pt_percentage'] = stats['twoPointFieldGoals']['pct']
             realized['3pt_percentage'] = stats['threePointFieldGoals']['pct']
             realized['ft_percentage'] = stats['freeThrows']['pct']
@@ -47,42 +50,58 @@ class Game:
                 realized['3pt_percentage'] *= realized['adjustment_factor']
                 realized['tov_rate'] *= realized['adjustment_factor']
                 realized['offensive_rebound_rate'] *= realized['adjustment_factor']
+                realized['ft_rate'] *= realized['adjustment_factor']
 
     def simulate_game(self, period_cnt=2, adjusted=False):
         self.get_data()
         self.calculate_adjustment_factor()
         self.calculate_realized_stats(adjusted)
+        print("Realized stats for simulation:")
+        for team in self.teams:
+            print(f"{team.name}: {self.realized_stats[team.name]}")
         self.play_by_play.append(f"Start of game: {self.teams[0].name} vs {self.teams[1].name}")
         for period in range(period_cnt):
             self.play_by_play.append(f"Start of period {period + 1}")
             start_team = random.choice(self.teams)
             self.simulate_period(start_team, period_length=20*60)
             start_team = self.teams[1] if start_team == self.teams[0] else self.teams[0]
+        while self.score[self.teams[0].name] == self.score[self.teams[1].name]:
+            self.play_by_play.append("Start of overtime")
+            self.simulate_period(start_team, period_length=5*60)
+        self.play_by_play.append("End of game")
         print(f"Final Score: {self.teams[0].name} {self.score[self.teams[0].name]} - {self.teams[1].name} {self.score[self.teams[1].name]}")
 
     def simulate_period(self, team_start, period_length=20*60):
         time_left = period_length
         attack_team = team_start
         while time_left > 0:
+            self.play_by_play.append(f"\nCurrent attacking team: {attack_team.name}, time left in period: {time_left:.2f} seconds")
+            current_attacking_team = attack_team.name
             result, possesion_time = self.simulate_play(attack_team, time_left)
             time_left -= possesion_time
             if result == 'turnover':
+                self.game_stats[attack_team.name]['tov'] += 1
                 attack_team = self.teams[1] if attack_team == self.teams[0] else self.teams[0]
-            elif result in ['2pt_made', '3pt_made', 'ft_made']:
-                if result == '2pt_made':
-                    self.score[attack_team.name] += 2
-                elif result == '3pt_made':
-                    self.score[attack_team.name] += 3
-                elif result == 'ft_made':
-                    self.score[attack_team.name] += 1
+            elif result in ['2pt_made', '3pt_made']:
+                points = 2 if result == '2pt_made' else 3
+                self.score[attack_team.name] += points
+                self.game_stats[attack_team.name][f'{result[0]}pt_made'] += 1
+                attack_team = self.teams[1] if attack_team == self.teams[0] else self.teams[0]
+            elif result.endswith('ft_made'):
+                ft_made = int(result.split()[0])
+                self.score[attack_team.name] += ft_made
+                self.game_stats[attack_team.name]['ft_made'] += ft_made
                 attack_team = self.teams[1] if attack_team == self.teams[0] else self.teams[0]
             elif result == 'offensive_rebound':
-                continue
+                self.game_stats[attack_team.name]['off_rebounds'] += 1
             elif result == 'missed':
                 attack_team = self.teams[1] if attack_team == self.teams[0] else self.teams[0]
-            self.play_by_play.append(f"{attack_team.name} {result} with {time_left:.2f} seconds left in period")
+                self.game_stats[attack_team.name]['def_rebounds'] += 1
+            self.play_by_play.append(f"{current_attacking_team} {result} with {time_left:.2f} seconds left in period")
+            self.play_by_play.append(f"Current score: {self.teams[0].name} {self.score[self.teams[0].name]} - {self.teams[1].name} {self.score[self.teams[1].name]}")
 
     def simulate_play(self, attack_team, time_left):
+        print("attacking team", attack_team.name)
         tov_rate = self.realized_stats[attack_team.name]['tov_rate']
         possession_time = self.realized_stats[attack_team.name]['possession_time']
         success_rate_mul = 1.0
@@ -93,15 +112,38 @@ class Game:
             possession_time = time_left
         if random.random() < tov_rate:
             return 'turnover', possession_time
-        shot_type = random.choices(['2pt', '3pt', 'ft'], weights=[self.realized_stats[attack_team.name]['2pt_distribution'], self.realized_stats[attack_team.name]['3pt_distribution'], self.realized_stats[attack_team.name]['ft_distribution']])[0]
+        shot_type = random.choices(['2pt', '3pt'], weights=[self.realized_stats[attack_team.name]['2pt_distribution'], self.realized_stats[attack_team.name]['3pt_distribution']])[0]
+        print("shot_type", shot_type)
         success_rate = self.realized_stats[attack_team.name][f'{shot_type}_percentage'] * success_rate_mul
-        if random.random() < success_rate:
+        fouled_rate = self.realized_stats[attack_team.name]['ft_rate'] * (1.0/2) * self.realized_stats[attack_team.name]['2pt_distribution'] + self.realized_stats[attack_team.name]['ft_rate'] * (1.0/3) * self.realized_stats[attack_team.name]['3pt_distribution']
+        print("fouled_rate", fouled_rate)
+        print("success_rate", success_rate)
+        if random.random() * 100 < fouled_rate:
+            print("Fouled!")
+            success_rate = self.realized_stats[attack_team.name]['ft_percentage']
+            if shot_type == '2pt':
+                # shoot 2 free throws
+                ft_made = sum(random.random() * 100 < success_rate for _ in range(2))
+                self.game_stats[attack_team.name]['ft_attempted'] += 2
+                return f"{ft_made} ft_made", possession_time
+            elif shot_type == '3pt':
+                # shoot 3 free throws
+                ft_made = sum(random.random() * 100 < success_rate for _ in range(3))
+                self.game_stats[attack_team.name]['ft_attempted'] += 3
+                return f"{ft_made} ft_made", possession_time
+        # Not account for and-1 situations
+        elif random.random() * 100 < success_rate:
+            print(f"{shot_type} shot made!")
+            self.game_stats[attack_team.name][f'{shot_type}_attempted'] += 1
             return f'{shot_type}_made', possession_time
         else:
+            print(f"{shot_type} shot missed.")
             offensive_rebound_rate = self.realized_stats[attack_team.name]['offensive_rebound_rate']
-            if random.random() < offensive_rebound_rate:
+            self.game_stats[attack_team.name][f'{shot_type}_attempted'] += 1
+            print("offensive_rebound_rate", offensive_rebound_rate)
+            if random.random() * 100 < offensive_rebound_rate:
                 return 'offensive_rebound', possession_time
-            else:                
+            else:
                 return 'missed', possession_time
 
 
--- src/main.py
+++ src/main.py
@@ -17,11 +17,35 @@ def main():
         print("Check your API key, BASE_URL, and that the API is returning valid data.")
         return
 
-    game = Game(t1, t2, 2026)
-    game.simulate_game(period_cnt=2, adjusted=True)
-
-    final_score = f"Final Score: {t1.name} {game.score[t1.name]} - {t2.name} {game.score[t2.name]}"
-    print(final_score)
+    t1_wins = 0
+    t2_wins = 0
+    for iter in range(100):
+        print(iter)
+        game = Game(t1, t2, 2026)
+        game.simulate_game(period_cnt=2, adjusted=True)
+        final_score = f"Final Score: {t1.name} {game.score[t1.name]} - {t2.name} {game.score[t2.name]}"
+        raw_game_stats = game.game_stats
+        # print(raw_game_stats)
+        game_stats = f"""
+Game Stats:
+2-pt: {t1.name} {game.game_stats[t1.name]['2pt_made']}/{game.game_stats[t1.name]['2pt_attempted']} ({game.game_stats[t1.name]['2pt_made'] / game.game_stats[t1.name]['2pt_attempted']:.1%}) | {t2.name} {game.game_stats[t2.name]['2pt_made']}/{game.game_stats[t2.name]['2pt_attempted']} ({game.game_stats[t2.name]['2pt_made'] / game.game_stats[t2.name]['2pt_attempted']:.1%})
+3-pt: {t1.name} {game.game_stats[t1.name]['3pt_made']}/{game.game_stats[t1.name]['3pt_attempted']} ({game.game_stats[t1.name]['3pt_made'] / game.game_stats[t1.name]['3pt_attempted']:.1%}) | {t2.name} {game.game_stats[t2.name]['3pt_made']}/{game.game_stats[t2.name]['3pt_attempted']} ({game.game_stats[t2.name]['3pt_made'] / game.game_stats[t2.name]['3pt_attempted']:.1%})
+ft: {t1.name} {game.game_stats[t1.name]['ft_made']}/{game.game_stats[t1.name]['ft_attempted']} ({game.game_stats[t1.name]['ft_made'] / game.game_stats[t1.name]['ft_attempted']:.1%}) | {t2.name} {game.game_stats[t2.name]['ft_made']}/{game.game_stats[t2.name]['ft_attempted']} ({game.game_stats[t2.name]['ft_made'] / game.game_stats[t2.name]['ft_attempted']:.1%})
+turnovers: {t1.name} {game.game_stats[t1.name]['tov']} | {t2.name} {game.game_stats[t2.name]['tov']}
+offensive rebounds: {t1.name} {game.game_stats[t1.name]['off_rebounds']} | {t2.name} {game.game_stats[t2.name]['off_rebounds']}
+defensive rebounds: {t1.name} {game.game_stats[t1.name]['def_rebounds']} | {t2.name} {game.game_stats[t2.name]['def_rebounds']}
+        """
+        # print(game_stats)
+        print(final_score)
+        # print(game_stats)
+        if game.score[t1.name] > game.score[t2.name]:
+            # print(f"{t1.name} wins!")
+            t1_wins += 1
+        elif game.score[t1.name] < game.score[t2.name]:
+            # print(f"{t2.name} wins!")
+            t2_wins += 1
+    
+    print(f"{t1.name} wins: {t1_wins} times, {t2.name} wins: {t2_wins} times")
     # print("\nPlay by Play Log:")
     # for log in game.play_by_play:
     #     print(log)
